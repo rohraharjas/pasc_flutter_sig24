c3595fb854627238dcab8ba81457816ce40c6771
chanekarayush <chanekarayush@gmail.com>
2024-03-06 22:28:22 +0530

    update app logic

--- tic_tac_toe_app/lib/main.dart
+++ tic_tac_toe_app/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,271 +15,217 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.red.shade300,
         cardColor: Colors.amber,
       ),
+      home: const HomePage(),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  bool oTurn = true;
-  List<String> displayElement = ['', '', '', '', '', '', '', '', ''];
-  int oScore = 0; // Score of O
-  int xScore = 0; // Score of X
-  int filledBoxes = 0;
+class _HomePageState extends State<HomePage> {
+  List<int> board = List.generate(9, (index) => index);
+  final List<int> _player1 = [];
+  final List<int> _player2 = [];
+  List<int> winningTile = [];
+  bool isPlayer1 = true;
+  bool? isPlayer1Won;
 
-  void _tapped(int index) {
-    setState(() {
-      if (oTurn && displayElement[index] == '') {
-        displayElement[index] = 'O';
-        filledBoxes++;
-      } else if (!oTurn && displayElement[index] == '') {
-        displayElement[index] = 'X';
-        filledBoxes++;
+  int player1WinCount = 0;
+  int player2WinCount = 0;
+
+  List<List<int>> winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  bool isWinner(List<int> playerTiles) {
+    for (var combo in winningCombinations) {
+      if (combo.every((element) => playerTiles.contains(element))) {
+        winningTile = combo;
+        return true;
       }
-
-      oTurn = !oTurn;
-      _checkWinner();
-    });
-  }
-
-  void _checkWinner() {
-    // Checking rows
-    if (displayElement[0] == displayElement[1] &&
-        displayElement[0] == displayElement[2] &&
-        displayElement[0] != '') {
-      _showWinDialog(displayElement[0]);
     }
-    if (displayElement[3] == displayElement[4] &&
-        displayElement[3] == displayElement[5] &&
-        displayElement[3] != '') {
-      _showWinDialog(displayElement[3]);
-    }
-    if (displayElement[6] == displayElement[7] &&
-        displayElement[6] == displayElement[8] &&
-        displayElement[6] != '') {
-      _showWinDialog(displayElement[6]);
-    }
-
-    // Checking Column
-    if (displayElement[0] == displayElement[3] &&
-        displayElement[0] == displayElement[6] &&
-        displayElement[0] != '') {
-      _showWinDialog(displayElement[0]);
-    }
-    if (displayElement[1] == displayElement[4] &&
-        displayElement[1] == displayElement[7] &&
-        displayElement[1] != '') {
-      _showWinDialog(displayElement[1]);
-    }
-    if (displayElement[2] == displayElement[5] &&
-        displayElement[2] == displayElement[8] &&
-        displayElement[2] != '') {
-      _showWinDialog(displayElement[2]);
-    }
-
-    // Checking Diagonal
-    if (displayElement[0] == displayElement[4] &&
-        displayElement[0] == displayElement[8] &&
-        displayElement[0] != '') {
-      _showWinDialog(displayElement[0]);
-    }
-    if (displayElement[2] == displayElement[4] &&
-        displayElement[2] == displayElement[6] &&
-        displayElement[2] != '') {
-      _showWinDialog(displayElement[2]);
-    } else if (filledBoxes == 9) {
-      _showDrawDialog();
-    }
-  }
-
-  void _showWinDialog(String winner) {
-    showDialog(
-        barrierDismissible: false,
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Center(
-              child: Text(" $winner is the Winner!!!"),
-            ),
-            actions: [
-              TextButton(
-                child: const Text("Play Again"),
-                onPressed: () {
-                  _clearBoard();
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        });
-
-    if (winner == 'O') {
-      oScore++;
-    } else if (winner == 'X') {
-      xScore++;
-    }
-  }
-
-  void _showDrawDialog() {
-    showDialog(
-        barrierDismissible: false,
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Center(
-              child: Text("It's a Draw"),
-            ),
-            actions: [
-              TextButton(
-                child: const Text("Play Again"),
-                onPressed: () {
-                  _clearBoard();
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        });
-  }
-
-  void _clearBoard() {
-    setState(() {
-      for (int i = 0; i < 9; i++) {
-        displayElement[i] = '';
-      }
-    });
-
-    filledBoxes = 0;
-  }
-
-  void _clearScoreBoard() {
-    setState(() {
-      xScore = 0;
-      oScore = 0;
-      for (int i = 0; i < 9; i++) {
-        displayElement[i] = '';
-      }
-    });
-    filledBoxes = 0;
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 15.0,
-                ),
-                padding: const EdgeInsets.symmetric(
-                  vertical: 30.0,
-                  horizontal: 15.0,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text(
-                      'Player X',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    Text(
-                      '$xScore',
-                      style: const TextStyle(fontSize: 20, color: Colors.white),
-                    ),
-                  ],
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        margin: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 5.0),
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 5,
+                    mainAxisSpacing: 5,
+                    childAspectRatio: 1,
+                  ),
+                  itemCount: board.length,
+                  itemBuilder: (context, index) {
+                    int element = board[index];
+                    return GestureDetector(
+                      child: Card(
+                        color: Theme.of(context).cardColor,
+                        elevation: 4.0,
+                        child: Center(
+                          child: Text(
+                            !_player1.contains(element) &&
+                                    !_player2.contains(element)
+                                ? ''
+                                : _player1.contains(element)
+                                    ? 'x'
+                                    : 'o',
+                            style: TextStyle(
+                                fontSize: 60,
+                                color: isPlayer1Won == null
+                                    ? _player1.contains(element)
+                                        ? Colors.blue
+                                        : Colors.green
+                                    : (isPlayer1Won! &&
+                                                winningTile.contains(element) ||
+                                            !isPlayer1Won! &&
+                                                winningTile.contains(element))
+                                        ? Colors.red
+                                        : Colors.grey),
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        if (isPlayer1Won != null) {
+                          return;
+                        }
+                        if (_player1.contains(element) ||
+                            _player2.contains(element)) {
+                          return;
+                        } else {
+                          setState(() {
+                            isPlayer1
+                                ? _player1.add(element)
+                                : _player2.add(element);
+                            isPlayer1 = !isPlayer1;
+                          });
+
+                          if (isWinner(_player1)) {
+                            setState(() {
+                              isPlayer1Won = true;
+                              player1WinCount++;
+                            });
+                          } else if (isWinner(_player2)) {
+                            setState(() {
+                              isPlayer1Won = false;
+                              player2WinCount++;
+                            });
+                          }
+
+                          if (isPlayer1Won != null) {
+                            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'Player ${isPlayer1Won! ? '1' : '2'} Won this match!'),
+                              ),
+                            );
+                          }
+
+                          if (player1WinCount + player2WinCount == 3) {
+                            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'Player ${(player1WinCount > player2WinCount) ? '1' : (player2WinCount > player1WinCount) ? '2' : 'draw'} is the winner !!'),
+                              ),
+                            );
+                          }
+                        }
+                      },
+                    );
+                  },
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Column(
-                  children: <Widget>[
-                    const Text(
-                      'Player O',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    Text(
-                      '$oScore',
-                      style: const TextStyle(fontSize: 20, color: Colors.white),
-                    ),
-                  ],
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _player1.clear();
+                    _player2.clear();
+                    isPlayer1 = true;
+                    isPlayer1Won = null;
+                    winningTile = [];
+                  });
+                },
+                child: Text(
+                  'RESET',
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).scaffoldBackgroundColor),
                 ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Player 1',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '$player1WinCount',
+                          style: const TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.green,
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Player 2',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '$player2WinCount',
+                          style: const TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               )
             ],
           ),
-          //Tic Tac Toe Board
-          Expanded(
-            child: GridView.builder(
-              itemCount: 9,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-              ),
-              itemBuilder: ((context, index) => GestureDetector(
-                    onTap: () {
-                      _tapped(index);
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.amber.shade500,
-                        border: Border.all(
-                          color: Theme.of(context).scaffoldBackgroundColor,
-                          width: 5.0,
-                        ),
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          displayElement[index],
-                          style: TextStyle(
-                              color: Theme.of(context).scaffoldBackgroundColor,
-                              fontSize: 40,
-                              fontFamily: "ComicSans"),
-                        ),
-                      ),
-                    ),
-                  )),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(
-              vertical: 30.0,
-              horizontal: 15.0,
-            ),
-            child: ElevatedButton(
-              onPressed: () {
-                _clearBoard();
-              },
-              onLongPress: () {
-                _clearScoreBoard();
-              },
-              child: Text(
-                "Reset Score",
-                style:
-                    TextStyle(color: Theme.of(context).scaffoldBackgroundColor),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
